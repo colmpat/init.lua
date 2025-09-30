9387f56b97faa0c70538d0c8c7540c76710d7891
Colm <cplang@dons.usfca.edu>
2025-09-30 11:49:52 -0700

    remove bad duplicate harpoon config

--- after/plugin/harpoon.lua
+++ after/plugin/harpoon.lua
@@ -7,9 +7,6 @@ harpoon:setup()
 vim.keymap.set("n", "<leader>a", function() harpoon:list():add() end)
 vim.keymap.set("n", "<C-e>", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
 
-vim.keymap.set('n', '<leader>a', mark.add_file)
-vim.keymap.set('n', '<C-e>', ui.toggle_quick_menu)
-
 vim.keymap.set('n', '<C-j>', function() ui.nav_file(1) end)
 vim.keymap.set('n', '<C-k>', function() ui.nav_file(2) end)
 vim.keymap.set('n', '<C-l>', function() ui.nav_file(3) end)
