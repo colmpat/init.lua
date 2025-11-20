76b69c6b35157acee8102fbc075da68cfd2f6381
Colm Lang <clang@makenotion.com>
2025-11-20 13:54:55 -0800

    .

--- lsp/ts_lsp.lua
+++ lsp/ts_lsp.lua
@@ -5,6 +5,7 @@ return {
   filetypes = { 'javascript', 'javascriptreact', 'typescript', 'typescriptreact' },
   root_markers = {
     'module.json',
+    '.root-nvim-lsp-marker',
     { 'tsconfig.js', 'tsconfig.json', 'package.json' },
     '.git',
   },
