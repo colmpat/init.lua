8a73921b55b6f6b96f74e5a8d07ec3ceadb9b75f
Colm Lang <clang@makenotion.com>
2025-11-25 11:11:42 -0800

    update harpoon tabline formatting

--- after/plugin/harpoon.lua
+++ after/plugin/harpoon.lua
@@ -14,4 +14,33 @@ vim.keymap.set("n", "<leader>;", function() harpoon:list():select(4) end)
 vim.keymap.set("n", "<leader>h", function() harpoon:list():select(5) end)
 
 -- tabline
-require('harpoon-tabline').setup()
+shorten_list_item_names = function(list)
+    local counts = {}
+    local length = list:length()
+    for i = 1, length do
+        local list_item = list.items[i]
+        if list_item ~= nil then
+            local name = vim.fn.fnamemodify(list_item.value, ":t")
+            counts[name or ""] = (counts[name] or 0) + 1
+        end
+    end
+    local shortened = {}
+    for i = 1, length do
+        local file = list.items[i]
+        if file == nil then
+            table.insert(shortened, i, nil)
+        else
+            local name = vim.fn.fnamemodify(file.value, ":t")
+            if counts[name] == 1 then
+                table.insert(shortened, i, name)
+            else
+                -- Get parent directory and filename
+                local parent = vim.fn.fnamemodify(file.value, ":h:t")
+                table.insert(shortened, i, parent .. "/" .. name)
+            end
+        end
+    end
+    return shortened
+end
+
+require('harpoon-tabline').setup({ format_item_names: shorten_list_item_names })
