5b22b26f700b36804e9b6f657b1a3cfe965ce169
Colm Lang <clang@makenotion.com>
2025-12-16 12:38:32 -0800

    .

--- lsp/ts_lsp.lua
+++ lsp/ts_lsp.lua
@@ -1,6 +1,6 @@
 return {
   -- Command and arguments to start the server.
-  cmd = { 'typescript-language-server', '--stdio', '--tsserver-args=--max-old-space-size=32768' },
+  cmd = { 'typescript-language-server', '--stdio', '--max-old-space-size=32768' },
   -- Filetypes to automatically attach to.
   filetypes = { 'javascript', 'javascriptreact', 'typescript', 'typescriptreact' },
   root_markers = {
